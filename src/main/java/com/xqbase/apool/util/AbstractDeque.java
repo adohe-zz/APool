b90918ac478c15c763a1adf0bf35487f6100c796
TonyAdo <coolhzb@163.com>
2015-04-08 14:04:40 +0800

    abstract deque add implementation...

--- src/main/java/com/xqbase/apool/util/AbstractDeque.java
+++ src/main/java/com/xqbase/apool/util/AbstractDeque.java
@@ -2,6 +2,8 @@ package com.xqbase.apool.util;
 
 import java.util.AbstractQueue;
 import java.util.Deque;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * This class provides skeletal implementations of some {@link Deque}
@@ -20,63 +22,93 @@ public abstract class AbstractDeque<E> extends AbstractQueue<E> implements Deque
 
     @Override
     public void addLast(E e) {
-
+        if (!offerLast(e)) {
+            throw new IllegalStateException("Queue full");
+        }
     }
 
     @Override
     public boolean removeFirstOccurrence(Object o) {
+        for (Iterator<E> i = iterator(); i.hasNext(); ) {
+            if (i.next().equals(o)) {
+                i.remove();
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean removeLastOccurrence(Object o) {
+        for (Iterator<E> i = descendingIterator(); i.hasNext(); ) {
+            if (i.next().equals(o)) {
+                i.remove();
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public E removeFirst() {
-        return null;
+        E e = pollFirst();
+        if (e == null) {
+            throw new NoSuchElementException();
+        }
+        return e;
     }
 
     @Override
     public E removeLast() {
-        return null;
+        E e = pollLast();
+        if (e == null) {
+            throw new NoSuchElementException();
+        }
+        return e;
     }
 
     @Override
     public E getFirst() {
-        return null;
+        E e = peekFirst();
+        if (e == null) {
+            throw new NoSuchElementException();
+        }
+        return e;
     }
 
     @Override
     public E getLast() {
-        return null;
+        E e = peekLast();
+        if (e == null) {
+            throw new NoSuchElementException();
+        }
+        return e;
     }
 
     @Override
     public void push(E e) {
-
+        addFirst(e);
     }
 
     @Override
     public E pop() {
-        return null;
+        return removeFirst();
     }
 
     // java.util.Queue methods
 
     @Override
     public boolean offer(E e) {
-        return false;
+        return offerLast(e);
     }
 
     @Override
     public E poll() {
-        return null;
+        return pollFirst();
     }
 
     @Override
     public E peek() {
-        return null;
+        return peekFirst();
     }
 }
